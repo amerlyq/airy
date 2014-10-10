11e5fdd05b7cdc3c28110bbff2d12a819cca2fab
Amerlyq <amerlyq@gmail.com>
2014-10-10 23:26:54 +0300

    Add synergy

--- deploy/generate.sh
+++ deploy/generate.sh
@@ -6,8 +6,8 @@ if [ -z "$SCRIPT_DIR" ]; then echo "Error: SCRIPT_DIR"; exit 1; fi
 
 GENS="$SCRIPT_DIR/generate.d"
 
-
 if [ "${CURR_PLTF}" == "Linux" ]; then
     "$GENS/xsessionrc.gen"
     "$GENS/xresources.gen"
+    "$GENS/synergy.gen"
 fi
