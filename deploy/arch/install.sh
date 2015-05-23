076d97dae90244531abec5b7320f5268e4f7136c
Amerlyq <amerlyq@gmail.com>
2015-05-24 00:07:46 +0300

    ENH archlinux vbox console, ADD adb completion

--- deploy/arch/install.sh
+++ deploy/arch/install.sh
@@ -1,48 +1,69 @@
 #!/usr/bin/env bash
 source ~/.bash/functions.d/vbox && amScriptDir || exit $?
-cd "$SCRIPT_DIR"
-if [ -z "$1" ]; then squit "Need remove option"; fi
+source "$SCRIPT_DIR/vbox-env" || exit $?  # Import some variables
 
-# SEE:
-#   https://github.com/wopfel/archlinux-setup-vb
+case "$1" in
+-n) oNew="" ;;
+-r) oNew="$1" ;;
+-g) do_vm_run "$VNM" -y; squit ;;
+ *) squit "Need option '-r' or '-n'" ;;
+esac
 
-# Make that script to export some current VBox variables here to use them!
-./vbox-create.sh $1
+do_vm_off "$VNM" -y
+[ -n "$oNew" ] && "$SCRIPT_DIR/vbox-create.sh" $oNew
+do_vm_run "$VNM" -y -h
+do_sleep 8
 
-
-VBOXIP="$(ifconfig vboxnet0 | awk '/inet addr/{print substr($2,6)}')"
+VBOXIP="$(ifconfig $VBOX_NET | awk '/inet addr/{print substr($2,6)}')"
 DHCPIP_INN="$(VBoxManage list dhcpservers | awk '/^IP:/{print $NF}')"
 
-VNM="Arch64"
-SERIAL="/tmp/vbox-${VNM}S0"
-
-do_vm_run -y "$VNM"
-
-# Get scancodes in interactive: sudo showkey -s
-#   http://www.win.tue.nl/~aeb/linux/kbd/scancodes-14.html
-
+# :=  Press <Tab> to edit boot string, add 'console=ttyS0', then <Enter>
 # WARNING: May be affected by problem with timing!
-sleep 8
-# Press <Tab> to edit boot string, add 'console=ttyS0', then <Enter>
-VBoxManage controlvm "$VNM" keyboardputscancode 0f 8f 39 b9  # <Tab><Space>
-VBoxManage controlvm "$VNM" keyboardputscancode 2e ae 18 98 31 b1 1f 9f 18 98 26 a6 12 92  # 'console'
-VBoxManage controlvm "$VNM" keyboardputscancode 0d 8d 14 94 14 94 15 95 36 1f 9f b6 0b 8b  # '=ttyS0'
-VBoxManage controlvm "$VNM" keyboardputscancode 1c 9c  # Enter
+do_vm_keys "$VNM" << EOT
+0f 8f 39 b9  # <Tab><Space>
+2e ae 18 98 31 b1 1f 9f 18 98 26 a6 12 92  # 'console'
+0d 8d 14 94 14 94 15 95 36 1f 9f b6 0b 8b  # '=ttyS0'
+33 b3 02 82 02 82 06 86 03 83 0b 8b 0b 8b 31 b1 09 89  # ',115200n8'
+1c 9c  # Enter
+EOT
 
+# NOTE: Get scancodes interactively: sudo showkey -s
+#   http://www.win.tue.nl/~aeb/linux/kbd/scancodes-14.html
 # TODO: Think how to duplicate input/output from ttyS0 and tty0 showed on monitor.
+# >>> It simply don't work!
+# NOTE: tty0 don't work and tty1 used for autologin
+# 0f 8f 39 b9  # <Tab><Space>
+# 2e ae 18 98 31 b1 1f 9f 18 98 26 a6 12 92  # 'console'
+# 0d 8d 14 94 14 94 15 95 03 83              # '=tty2'
 
-sleep 25
-### Send commands:
-printf "root\n" | nc -q 2 -U "$SERIAL"                           # Login
-printf "\n"'cat - <<-'"'EOFARCHSETUP' > ~/arch-setup\n" | nc -U "$SERIAL"  # Send as heredoc
-cat "$SCRIPT_DIR/arch-setup" | nc -U "$SERIAL"                   # Send text content of setup file
-printf "\nEOFARCHSETUP\n" | nc -U "$SERIAL"                      # End of heredoc file
 
-printf "\nchmod u+x ~/arch-setup\n" | nc -U "$SERIAL"      # Make executable
-printf "\ncd ~ && ./arch-setup --new\n" | nc -U "$SERIAL"  # Start install
-nc -d -U "$SERIAL"  # Non-interactive monitoring of installation (safe Ctrl-C to interrupt)
-# ERROR: you must enter root password! How? Extract it from script and lauch interactive separately?
-# TODO: reboot after all
+## Login
+do_sleep 25 && do_vm_socket "$SERIAL" <<< "root"
+# Send setup files
+for fl in "$SCRIPT_DIR"/arch/*; do do_vm_socket_file "$SERIAL" "$fl"; done
+
+# Start installation
+do_vm_socket "$SERIAL" << EOT
+cd ~
+chmod u+x ./install
+./install ${oNew+--new}
+poweroff
+EOT
+
+nc -d -U "$SERIAL" | tee "${SERIAL}.log"
+wait $VBOX_PID
+# Non-interactive monitoring of installation (safe Ctrl-C to interrupt)
+# nc -U "$SERIAL"
+# nc -d -U "$SERIAL" #| awk '/^::: install complete :::$/{exit}'
+# Shutdown after all
+# do_vm_socket "$SERIAL" <<< "poweroff"
+
+# do_sleep 2
+## Unmount archiso from vbox and disable sata for it
+VBoxManage storageattach "$VNM" --storagectl "IDE" --port 0 --device 0 --medium emptydrive
+VBoxManage snapshot "$VNM" take "console-base" \
+    --description "Initial state with only 'base' package installed"
+
 
 # Netcat
 #   http://www.debian-administration.org/article/58/Netcat_The_TCP/IP_Swiss_army_knife
@@ -58,9 +79,6 @@ nc -d -U "$SERIAL"  # Non-interactive monitoring of installation (safe Ctrl-C to
 # ALT: use 'arp' to get MACs from vbox dhcp server, then identify IP of each guest by it's MAC
 # ALT: get host names? How? How synergy does it?
 # Crossplatform  ==  Linux + Windows + Geanymotion (Android)
-
-## First state after boot
-# VBoxManage controlvm <vm> savestate
 
 
 # Setup static ip for guests' Host-only network
@@ -81,9 +99,9 @@ nc -d -U "$SERIAL"  # Non-interactive monitoring of installation (safe Ctrl-C to
 #   VBoxManage guestproperty enumerate Arch64
 #   VBoxManage guestproperty get Arch64 "/VirtualBox/GuestInfo/Net/2/V4/IP"
 
-
-
 # ALT: Vagrant
 #   https://lwn.net/Articles/486319/
 # Send binary as text over serial port to bare linux embedded device
 #   http://www.devttys0.com/2011/05/serial-file-uploads-with-serio/
+# BADLY e.g: https://github.com/wopfel/archlinux-setup-vb
+
--- deploy/arch/vbox-create.sh
+++ deploy/arch/vbox-create.sh
@@ -1,52 +1,28 @@
 #!/usr/bin/env bash
-source ~/.bash_export || exit $?
-source ~/.bash/functions.d/vbox || exit $?
 ARGS="$@"
-
-VNM=Arch64
-ost=ArchLinux_64
-if [ ! "$ARGS" == "-w" ] && [ "$CURR_PLTF" == "Linux" ]
-then
-    sudo="sudo"
-    VMs="$HOME/VMs"
-    SERIAL="/tmp/vbox-${VNM}S0"
-else
-    sudo=""
-    VMs="${2:-/e/VMs}"
-    PATH="$PATH:/c/Program Files/Oracle/VirtualBox"
-    printf "\nYou need to run this script as Administrator!\n"
-    SERIAL='\\.\pipe\'"vbox-${VNM}S0"
-fi
-vimg="$VMs/${VNM}/${VNM}.vdi"
-vbox="${vimg%.*}.vbox"
-install_iso="$(find "$VMs" -type f -name "archlinux*iso")"
-VBOX_PORT=2822
-GUEST_NET=vboxnet0
+source ~/.bash/functions.d/vbox && amScriptDir || exit $?
+source "$SCRIPT_DIR/vbox-env" || exit $?
+printf "\n>>> ${VNM?No VM name} guest image <<<\n"
 
 # =======================================================================
-
-printf "\n>>> ${VNM} guest image <<<\n"
 
 if [ "$ARGS" == "-r" ]; then rm -rf "${vimg%/*}"; fi
 mkdir -p "${vimg%/*}"
 
 
-if is_vm_exist "$VNM"; then case "$ARGS" in
+if is_vm_exist "$VNM"; then
+    do_vm_off "$VNM" || squit "Rejected VM shutdown"
+    case "$ARGS" in
 -d|-r) VBoxManage unregistervm "${VNM}" ;; #--delete
--m) printf "VM exist, updating settings!\n" ;;
- *) squit "\n!!! ERR: There are already such VM: ${VNM} !!!\n";
+   -m) printf "VM exist, updating settings!\n" ;;
+    *) squit "\n!!! ERR: There are already such VM: ${VNM} !!!\n";
 esac; else case "$ARGS" in
--m) squit "There no such VM as '${VNM}'"
+   -m) squit "There no such VM as '${VNM}'"
 esac; fi
 
-if is_vm_run "$VNM"; then
-    if ask_confirm "Shutdown to modify?"
-    then VBoxManage controlvm "${VNM}" poweroff
-    else squit; fi
-fi
 
-if [ ! -f "$vbox" ]; then
-    VBoxManage createvm --name "${VNM}" --ostype "$ost" --basefolder "$VMs" --register
+if [ ! -f "${vbox?No *.vbox}" ]; then
+    VBoxManage createvm --name "${VNM}" --ostype "$ost" --basefolder "${VMs?No VMs dir}" --register
 
     case "$CURR_PROF" in  # 100MB recovery, Win7, Data
         "home") VMOPTS="--memory 1024" ;;
@@ -55,14 +31,17 @@ if [ ! -f "$vbox" ]; then
     # For Windows Guests only:  --accelerate2dvideo on
     case "$CURR_PLTF" in
         "Linux") VMOPTS="$VMOPTS --audio pulse  --audiocontroller hda  --accelerate2dvideo off"  ;;
-        "MINGW") VMOPTS="$VMOPTS --audio dsound --audiocontroller ac97 --accelerate2dvideo off" ;;
+        "MINGW") VMOPTS="$VMOPTS --audio dsound --audiocontroller ac97 --accelerate2dvideo on" ;;
     esac
 
+    ## Headless
+    # --vram 16 --accelerate3d off --clipboard disabled --monitorcount 1 --audio none --usb off --usbehci off
+
     # Add host-only network in Host vbox and check it:
-    if ! VBoxManage list -l hostonlyifs | grep -q "$GUEST_NET"; then
+    if ! VBoxManage list -l hostonlyifs | grep -q "${VBOX_NET?No vbox net}"; then
         VBoxManage hostonlyif create
-        VBoxManage dhcpserver add --ifname "$GUEST_NET" --enable
-        ifconfig "$GUEST_NET"
+        VBoxManage dhcpserver add --ifname "$VBOX_NET" --enable
+        ifconfig "$VBOX_NET"
     fi
 
     # (virtualization options Only for Intel Host)
@@ -71,26 +50,18 @@ if [ ! -f "$vbox" ]; then
         --hwvirtex on --nestedpaging on  --largepages on  --vtxvpid on \
         --boot1 dvd --boot2 disk --firmware bios  --accelerate3d on    \
         --clipboard bidirectional --monitorcount 1 --vrde off          \
-        --usb on --usbehci off --nic1 nat --nic2 hostonly --hostonlyadapter2 "${GUEST_NET}"
+        --usb on --usbehci off --nic1 nat --nic2 hostonly --hostonlyadapter2 "${VBOX_NET}"
     # Host-only: https://forums.virtualbox.org/viewtopic.php?f=8&t=34396
     # Intranet: --nic3 intnet  --intnet3 "InnerVMs"
-    add_port_forward "${VNM}" "${VNM}_ssh" "tcp,,$VBOX_PORT,,22"
+    add_port_forward "${VNM}" "${VNM}_ssh" "tcp,,${VBOX_PORT?No vbox port},,22"
 
     # Provide virtual serial port to debug guest kernel
     #   Install Arch from serial port:  https://wiki.archlinux.org/index.php/Working_with_the_serial_console
-    VBoxManage modifyvm "${VNM}" --uart1 0x3F8 4 --uartmode1 server "$SERIAL"
+    VBoxManage modifyvm "${VNM}" --uart1 0x3F8 4 --uartmode1 server "${SERIAL?No vbox serial}"
 
     # Only if Extensions installed
     # --usbehci on
     # --defaultfrontend default|<name>
-
-    ## Headless
-    # VBoxManage modifyvm "${VNM}" --memory 1024 --vram 16 --pae on      \
-    #     --ioapic on --cpus 2 --rtcuseutc on --cpuexecutioncap 100      \
-    #     --hwvirtex on --nestedpaging on  --largepages on  --vtxvpid on \
-    #     --boot1 dvd --boot2 disk --firmware bios  --accelerate3d off   \
-    #     --clipboard disabled --monitorcount 1 --vrde off --audio none  \
-    #     --usb off --usbehci off --nic1 nat --nic2 hostonly
 
     sed -i '/ShowMiniToolBar/ s/\(value="\)\w\+"/\1no"/' "$vbox"
 fi
@@ -105,10 +76,10 @@ if [ ! -f "$vimg" ]; then
         "work"|*) HDDOPTS="" ;;
     esac
 
-    VBoxManage storagectl "${VNM}" --name "IDE"  --add ide \
+    VBoxManage storagectl "${VNM}" --name "IDE" --add ide \
         --controller PIIX4 --hostiocache on --bootable on
     VBoxManage storageattach "${VNM}" --storagectl "IDE" \
-        --port 0 --device 0 --type dvddrive --medium "$install_iso"
+        --port 0 --device 0 --type dvddrive --medium "${install_iso?No *.iso to install}"
 
     VBoxManage createhd --filename "$vimg" \
         --size 20480 --format VDI --variant Standard
