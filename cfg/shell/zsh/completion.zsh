2064b537fd6f310ab4bade5fb0082063ee64c638
Amerlyq <amerlyq@gmail.com>
2015-12-16 13:03:41 +0200

    ENH git-recursive: added thread limitation, output grouping, autocompletion

--- cfg/shell/zsh/completion.zsh
+++ cfg/shell/zsh/completion.zsh
@@ -10,4 +10,13 @@ fpath=(~/.shell/zsh/completion.d $fpath)
 # zstyle ':completion:*' menu select=2
 
 ## To see it you may need to add the following line into your .zshrc:
-zstyle ":completion:*:descriptions" format "%B%d%b"
+# zstyle ":completion:*:descriptions" format "%B%d%b"
+
+# compdef prg.d/git-recursive=git
+compdef prg.d/git-recursive=git
+for c in st pl ph; do eval "function _git-$c { _git; }"; done
+
+# ALT?
+# autoload -Uz compinit && compinit
+# compdef _git ~/.bin/prg.d/git-recursive   # Use X completion for Y command
+# compdef '_dispatch git git' Gg          # Seems like must work for aliases
