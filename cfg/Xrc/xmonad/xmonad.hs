e859fcf8c36e9f449cd66044203ee91d4919afca
Amerlyq <amerlyq@gmail.com>
2016-02-10 20:57:12 +0200

    Cfg: xmonad, Fix: mpv, Note: awk, gdb

--- cfg/Xrc/xmonad/xmonad.hs
+++ cfg/Xrc/xmonad/xmonad.hs
@@ -1,5 +1,12 @@
+-- vim: ts=2:sw=2:sts=2
+
 -- >> https://ro-che.info/docs/xmonad/
 -- https://wiki.haskell.org/Xmonad
+-- http://xmonad.org/xmonad-docs/xmonad/XMonad-StackSet.html
+-- http://prajitr.github.io/quick-haskell-syntax/
+-- https://www.schoolofhaskell.com/school/to-infinity-and-beyond/pick-of-the-week/Simple%20examples
+-- https://en.wikibooks.org/wiki/Haskell/Indentation
+
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
@@ -21,30 +28,37 @@ myTerminal = "r.t"
 myWorkspaces = words "` 1 2 3 4 5 6 7 8 9 0 - ="
 
 myKeys = \conf -> mkKeymap conf $
-         [ ("M-u",          spawn       $ myTerminal )
-         , ("M-d",          spawn       $ "r.dmenu")
-         -- cycle through all windows
-         , ("M-j",          windows     $ W.focusDown)
-         , ("M-k",          windows     $ W.focusUp)
-         -- , ("M-S-q",        io          $ exitWith ExitSuccess)
-         , ("M-q",          restart "xmonad" True)
-         ]
-         ++
-         [ ( m ++ i, windows $ f i)
-            | i <- myWorkspaces
-            , (m, f) <- [("M-", W.view), ("M-S-", W.shift)]
-         ]
+  [ ("M-u",          spawn       $ myTerminal )
+  , ("M-d",          spawn       $ "r.dmenu")
+  -- cycle through all windows
+  , ("M-j",          windows     $ W.focusDown)
+  , ("M-k",          windows     $ W.focusUp)
+  -- , ("M-S-q",        io          $ exitWith ExitSuccess)
+  , ("M-q",          restart "xmonad" True)
+  ]
+  ++
+  [ ( m ++ i, windows $ f i)
+    | i <- myWorkspaces
+    , (m, f) <- [("M-", W.view), ("M-S-", W.shift)]
+  ]
+
+cfg = defaultConfig
+  { modMask = mod4Mask
+  -- Options
+  , terminal    = myTerminal
+  , workspaces  = myWorkspaces
+  , keys        = myKeys
+  -- Hooks
+  -- , startupHook = setWMName "LG3D"
+  , manageHook = manageDocks <+> manageHook defaultConfig
+  , layoutHook = avoidStruts  $  layoutHook defaultConfig
+  -- Style
+  , borderWidth        = 3
+  , normalBorderColor  = "#000000"
+  , focusedBorderColor = "#c050f0"
+  }
 
 main = do
-  xmproc <- spawnPipe "xmobar ~/.xmobarrc"
-  xmonad $ baseConfig
-    { modMask     = mod4Mask
-    -- Options
-    , terminal    = myTerminal
-    , workspaces  = myWorkspaces
-    , keys        = myKeys
-    -- Hooks
-    -- , startupHook = setWMName "LG3D"
-    , manageHook = manageDocks <+> manageHook defaultConfig
-    , layoutHook = avoidStruts  $  layoutHook defaultConfig
-    }
+    xmproc <- spawnPipe "xmobar" -- ~/.xmobarrc
+    xmonad $ cfg
+    print $ cfg.terminal
