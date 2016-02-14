b7dc2f792948ac2675883508a1e1925bec9a2a2c
Amerlyq <amerlyq@gmail.com>
2016-02-14 19:39:00 +0200

    Cfg: xmonad

--- cfg/Xrc/xmonad/xmonad.hs
+++ cfg/Xrc/xmonad/xmonad.hs
@@ -1,16 +1,19 @@
 -- vim: ts=2:sw=2:sts=2
+module Main (main) where
 
 import XMonad
 import XMonad.Config.Desktop
 
 import XMonad.Hooks.DynamicLog      (statusBar, xmobarPP, xmobarColor, PP(..))
-import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageDocks     (manageDocks, avoidStruts, docksEventHook)
+import XMonad.Layout.LayoutHints    (hintsEventHook)  -- honor size hints
 -- import XMonad.Hooks.SetWMName
 
 import XMonad.Util.EZConfig         (additionalKeys, mkKeymap)
+import Graphics.X11.ExtraTypes.XorgDefault  -- xK_ISO_Left_Tab
 
 import qualified XMonad.StackSet as W
-import XMonad.Actions.CycleRecentWS
+import XMonad.Actions.CycleRecentWS (cycleWindowSets)
 
 import System.IO
 import System.Exit
@@ -19,34 +22,28 @@ import System.Exit
 myKeys conf = mkKeymap conf $
   [ ("M-u"        , spawn $ terminal myCfg)
   , ("M-d"        , spawn "r.dmenu"       )
-  -- workspaces
-  -- -- Cycle recent (not visible) workspaces, tab is next, escape previous in history
-  -- , let options w     = map (W.greedyView `flip` w)   (hiddenTags w)
-  --   in ("M-<Tab>" , cycleWindowSets options [xK_Super_L] xK_Tab xK_Escape)
-  -- -- Cycle through visible screens, a is next, s previous
-  -- , let options w     = map (W.view `flip` w)         (visibleTags w)
-  --   in ("M-a"     , cycleWindowSets options [xK_Super_L] xK_a xK_s)
-  -- Swap visible workspaces on current screen, s is next, d previous
-  -- , let options w     = map (W.greedyView `flip` w)   (visibleTags w)
-  --   in ("M-s"     , cycleWindowSets options [xK_Super_L] xK_s xK_d)
-  , ("M-<Tab>"    , myCycleRecentWS)  -- Toggle previously displayed workspaces
   -- cycle through all windows
   , ("M-j"        , windows W.focusDown)
   , ("M-k"        , windows W.focusUp)
   -- Xmonad
   , ("M-q"          , restart "xmonad" True)
   , ("M-S-<Escape>" , io exitSuccess)
-  ]
-  ++
-  [ ( m ++ i, windows $ f i)
-    | i <- (workspaces myCfg)
+  ] ++
+  -- cycling
+  let visWs w = map (W.greedyView `flip` w) (visTags w)
+      hidWs w = map (W.greedyView `flip` w) (hidTags w)
+      rctWs w = map (W.view `flip` w)       (map ($ hidTags w) [head, last])
+  in [ ("M-<Tab>"  , cycleWindowSets visWs [xK_Super_L] xK_Tab xK_ISO_Left_Tab)
+     , ("M-S-<Tab>", cycleWindowSets hidWs [xK_Super_L] xK_ISO_Left_Tab xK_Tab)
+     , ("M-a"      , cycleWindowSets rctWs [xK_Super_L] xK_a xK_a)
+  ] ++
+  -- workspaces
+  [ (m ++ i, windows $ f i) | i <- workspaces myCfg
     , (m, f) <- [("M-", W.view), ("M-S-", W.shift)]
   ]
   where
-    hiddenTags w    = map W.tag $ W.hidden w ++ [W.workspace . W.current $ w]
-    visibleTags w   = map (W.tag . W.workspace) $ W.visible w ++ [W.current w]
-    myCycleRecentWS = let options w = map (W.view `flip` w) (hiddenTags w)
-                      in cycleWindowSets options [xK_Super_L] xK_Tab xK_q
+    hidTags w = map W.tag $ W.hidden w ++ [W.workspace . W.current $ w]
+    visTags w = map (W.tag . W.workspace) $ W.visible w ++ [W.current w]
 
 myCfg = defaultConfig
   { modMask = mod4Mask
@@ -56,8 +53,10 @@ myCfg = defaultConfig
   , keys        = myKeys
   -- Hooks
   -- , startupHook = setWMName "LG3D"
-  , manageHook = manageDocks <+> manageHook defaultConfig
+  , manageHook = manageHook defaultConfig <+> manageDocks
   , layoutHook = avoidStruts  $  layoutHook defaultConfig
+  , handleEventHook = handleEventHook defaultConfig <+> mconcat
+      [ docksEventHook, hintsEventHook ]
   -- Style
   , borderWidth        = 2
   , normalBorderColor  = "#000000"
