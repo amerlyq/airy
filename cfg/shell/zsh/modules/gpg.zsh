c06eb3c7bd494cc1240d658044256ceb55e29364
Amerlyq <amerlyq@gmail.com>
2016-06-27 19:03:07 +0300

    Fix: gpg-agent/ssh

--- cfg/shell/zsh/modules/gpg.zsh
+++ cfg/shell/zsh/modules/gpg.zsh
@@ -1,22 +1,23 @@
 ### SSH-agent ###
 (( $+commands[gpg-agent] )) || return
 
-
-export GPG_TTY=$(tty)
-
+# DISABLED:(use gpg.service instead)
 # Start the gpg-agent if not already running
-if ! pgrep -x -u "$USER" gpg-agent >/dev/null 2>&1; then
-    gpg-connect-agent /bye >/dev/null 2>&1
-fi
-unset SSH_AGENT_PID
+# if ! pgrep -x -u "$USER" gpg-agent >/dev/null 2>&1; then
+#     gpg-connect-agent /bye >/dev/null 2>&1
+# fi
+# unset SSH_AGENT_PID
 
 # SSH_AUTH_SOCK=$(gpg-connect-agent 'getinfo ssh_socket_name' /bye | awk '/^D/{print $2}')
 # [[ -S "$SSH_AUTH_SOCK" ]] && export SSH_AUTH_SOCK
-if [[ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]]; then
-    export SSH_AUTH_SOCK=~/.gnupg/S.gpg-agent.ssh
+if [[ ${gnupg_SSH_AUTH_SOCK_by:-0} -ne $$ ]]; then
+    SSH_AUTH_SOCK=/run/user/${UID:-1000}/gnupg/S.gpg-agent.ssh
+    [[ -S $SSH_AUTH_SOCK ]] || SSH_AUTH_SOCK=~/.gnupg/S.gpg-agent.ssh
+    export SSH_AUTH_SOCK
 fi
 
 # Refresh gpg-agent tty in case user switches into an X session
+export GPG_TTY=$(tty)
 gpg-connect-agent updatestartuptty /bye >/dev/null
 
 #ALT# Updates the GPG-Agent TTY before every command since SSH does not set it.
@@ -26,4 +27,4 @@ gpg-connect-agent updatestartuptty /bye >/dev/null
 # add-zsh-hook preexec _gpg-agent-update-tty
 
 # Disable GUI prompts inside SSH.
-[[ -n "$SSH_CONNECTION" ]] || export PINENTRY_USER_DATA='USE_CURSES=1'
+[[ -n $SSH_CONNECTION ]] || export PINENTRY_USER_DATA='USE_CURSES=1'
