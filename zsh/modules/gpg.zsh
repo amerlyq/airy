50091e15130af68df87011b6b9c8ea402aa8e759
Amerlyq <amerlyq@gmail.com>
2017-07-11 04:18:59 +0300

    [gpg] use curses pineentry in all tty

--- zsh/modules/gpg.zsh
+++ zsh/modules/gpg.zsh
@@ -11,20 +11,20 @@
 # SSH_AUTH_SOCK=$(gpg-connect-agent 'getinfo ssh_socket_name' /bye | awk '/^D/{print $2}')
 # [[ -S "$SSH_AUTH_SOCK" ]] && export SSH_AUTH_SOCK
 if [[ ${gnupg_SSH_AUTH_SOCK_by:-0} -ne $$ ]]; then
-    SSH_AUTH_SOCK=/run/user/${UID:-1000}/gnupg/S.gpg-agent.ssh
-    [[ -S $SSH_AUTH_SOCK ]] || SSH_AUTH_SOCK=~/.gnupg/S.gpg-agent.ssh
-    export SSH_AUTH_SOCK
+  SSH_AUTH_SOCK=/run/user/${UID:-1000}/gnupg/S.gpg-agent.ssh
+  [[ -S $SSH_AUTH_SOCK ]] || SSH_AUTH_SOCK=~/.gnupg/S.gpg-agent.ssh
+  export SSH_AUTH_SOCK
 fi
 
-# Refresh gpg-agent tty in case user switches into an X session
-export GPG_TTY=$(tty)
-gpg-connect-agent updatestartuptty /bye >/dev/null
+if tty -s; then
+  # Refresh gpg-agent tty in case user switches into an X session
+  export GPG_TTY=$(tty)
+  # Disable GUI prompts inside SSH.
+  # [[ -n $SSH_CONNECTION ]] ||
+  export PINENTRY_USER_DATA='USE_CURSES=1'
 
-#ALT# Updates the GPG-Agent TTY before every command since SSH does not set it.
-# function _gpg-agent-update-tty {
-#   gpg-connect-agent UPDATESTARTUPTTY /bye >/dev/null
-# }
-# add-zsh-hook preexec _gpg-agent-update-tty
-
-# Disable GUI prompts inside SSH.
-[[ -n $SSH_CONNECTION ]] || export PINENTRY_USER_DATA='USE_CURSES=1'
+  # CHECK:BUG: correctly work only for last logined ssh
+  #   MAYBE:FIX: use 'add-zsh-hook preexec ...' to update before every command?
+  # https://unix.stackexchange.com/questions/280879/how-to-get-pinentry-curses-to-start-on-the-correct-tty
+  gpg-connect-agent updatestartuptty /bye >/dev/null
+fi
