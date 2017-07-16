41423e7ce24293cf736ce0f7961546539b0dd4c9
Amerlyq <amerlyq@gmail.com>
2017-07-16 04:07:15 +0300

    fixup

--- zsh/filetypes.zsh
+++ zsh/filetypes.zsh
@@ -1,7 +1,10 @@
 # vim: ft=zsh
 
+# WARN: don't use {mk,c,cpp,h,hpp}
+#   -- I often use source files as self-executing test examples
+
 alias -s {avi,mpeg,mpg,mov,mkv}=mpv
-alias -s {txt,c,cpp,h,hpp}="$EDITOR"
+alias -s {nou,txt}="$EDITOR"
 alias -s {html,php}='\w3m -T text/html'
 alias -s {png,jpg,gif}=pqiv
 alias -s {djvu,pdf}=zathura
