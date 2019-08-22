24460dc59388738260ba8588830b4bf0b292b35f
Amerlyq <amerlyq@gmail.com>
2019-08-22 16:48:52 +0300

    [pass] use tomb

--- zsh/filetypes.zsh
+++ zsh/filetypes.zsh
@@ -4,7 +4,7 @@
 #   -- I often use source files as self-executing test examples
 
 alias -s {avi,mpeg,mpg,mov,mkv}=mpv
-alias -s {nou,txt}="$EDITOR"
+alias -s {nou,otl}="$EDITOR"
 alias -s {html,php}='\w3m -T text/html'
 alias -s {png,jpg,gif}=pqiv
 alias -s {djvu,pdf}=zathura
