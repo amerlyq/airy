2913bdff7111074c6ba9edff4fca44827497e623
Amerlyq <amerlyq@gmail.com>
2020-07-20 04:04:36 +0300

    [vis] cfg theme

---
+++ vis/visrc.lua
@@ -0,0 +1,12 @@
+-- load standard vis module, providing parts of the Lua API
+require('vis')
+
+vis.events.subscribe(vis.events.INIT, function()
+	-- Your global configuration options
+	vis:command('set theme solarized')
+end)
+
+vis.events.subscribe(vis.events.WIN_OPEN, function(win)
+	-- Your per window configuration options e.g.
+	-- vis:command('set number')
+end)
