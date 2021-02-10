8623ec5f655bba636b11c8652113d6d8041bcd7e
Amerlyq <amerlyq@gmail.com>
2021-02-10 21:05:05 +0200

    [ranger] * chg sorting_similar.py  +9/-6

--- ranger/plugins/sorting_similar.py
+++ ranger/plugins/sorting_similar.py
@@ -5,21 +5,24 @@ from os import path as fs
 
 from ranger.container.directory import Directory
 
-Directory.sort_dict['from_end'] = lambda x: x.relative_path[::-1]
+Directory.sort_dict["from_end"] = lambda x: x.relative_path[::-1]
+Directory.sort_dict["name_len"] = lambda x: (len(x.relative_path), x.relative_path)
 
 # TODO:ALSO:(o,|o.): use rest/tail after _any_ punct symbol
 # THINK: how to sort mixed dir with both "a-b" and "b" -- mix or separate
 #   t = nm(x).rpartition('-');
 
+
 def sort_suffix(x, s, longest=False):
     nm = x.relative_path
     t = nm.partition(s) if longest else nm.rpartition(s)
-    return t[2] if t[1] else ''
+    return t[2] if t[1] else ""
 
-Directory.sort_dict['suffix--'] = lambda x: sort_suffix(x, '-', longest=True)
-Directory.sort_dict['suffix-'] = lambda x: sort_suffix(x, '-', longest=False)
-Directory.sort_dict['suffix__'] = lambda x: sort_suffix(x, '_', longest=True)
-Directory.sort_dict['suffix_'] = lambda x: sort_suffix(x, '_', longest=False)
+
+Directory.sort_dict["suffix--"] = lambda x: sort_suffix(x, "-", longest=True)
+Directory.sort_dict["suffix-"] = lambda x: sort_suffix(x, "-", longest=False)
+Directory.sort_dict["suffix__"] = lambda x: sort_suffix(x, "_", longest=True)
+Directory.sort_dict["suffix_"] = lambda x: sort_suffix(x, "_", longest=False)
 
 # from subprocess import check_output
 # def sort_dir_size(path):
