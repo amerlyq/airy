a40625428b3967da13a159ae9e2a0e43fe8171c3
Amerlyq <amerlyq+git@gmail.com>
2022-06-21 22:46:51 +0300

    [airy] fst cfg

---
+++ nvim/_.py
@@ -0,0 +1,20 @@
+from just.airy import M
+# pkgs = ["neovim"]
+pkgs = []
+
+# VIZ. $ nvr to talk to vim from inside ranger
+aurs = ["neovim-git", "neovim-remote"]
+
+
+# DEPS:(aur/python-lsp-all):
+#   python-pylsp-rope ALSO: pip install pylsp-autoimport && auri python-autoimport
+pkgs += ["python-pylint", "python-lsp-server", "python-lsp-black"]
+aurs += ["python-pylsp-mypy", "python-pyls-isort"]  # python-lsp-isort
+# pip_inst pyls-isort
+
+pkgs += ["lua-language-server"]
+
+## ALT: don't use symlinks -- always copy whole file, and then install it by PKGBUILD
+links = [(Path.cwd(), "~/.config/nvim")]
+
+# TODO: mkdir /@/audit/$HOST/vim
