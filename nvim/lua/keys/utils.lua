b877871f9001887f4c3717b5ebfe59b3127aa348
Amerlyq <amerlyq+git@gmail.com>
2022-07-22 23:56:50 +0300

    [nvim] * chg utils.lua  +4

--- nvim/lua/keys/utils.lua
+++ nvim/lua/keys/utils.lua
@@ -8,6 +8,10 @@ K('n', ',tl', "<Cmd>let &showtabline=(&stal<2?2:1)<CR>")
 K('n', ',tw', "<Cmd>setl wrap! wrap?<CR>")
 
 
+K('n', '\\q', "<Cmd>copen<CR>")
+K('n', '\\Q', "<Cmd>lopen<CR>")
+-- K('n', '\\Q', "<Cmd>cclose<CR>")
+
 
 -- K('n', '<F3>', [[
 -- :echo 'hi<'.synIDattr(synID(line('.'), col('.'), 1), 'name')
