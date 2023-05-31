3cfdb6629a36d8e9f742260a426ec3c8548f44c6
Amerlyq <amerlyq+git@gmail.com>
2023-05-31 16:54:49 +0300

    [%] * chg init,(+8): 9 files  +36/-8

--- systemd/_.py
+++ systemd/_.py
@@ -1,4 +1,7 @@
 from just.airy.api import ln, cp, Pkg
 
+# sudo systemctl enable --now earlyoom.service
+Pkg("earlyoom")
+
 # FIXED※⡢⢳⡪⠒ stop spamming journal on each sudo
 cp("cfg/system-auth" , under="/etc/pam.d")
