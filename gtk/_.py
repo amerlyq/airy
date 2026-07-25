3235c17a1e1d9ba9b2ef7bd90dd219a26d33d358
Amerlyq <amerlyq+git@gmail.com>
2026-07-25 23:44:30 +0300

    [%] * chg ranger,(+5): 8 files  +115/-9

--- gtk/_.py
+++ gtk/_.py
@@ -1,3 +1,4 @@
 from airy.api import cp
 
 cp("settings.ini", under="~/.config/gtk-3.0")
+cp("settings.ini", under="~/.config/gtk-4.0")
--- ranger/plugins/sorting_similar.py
+++ ranger/plugins/sorting_similar.py
@@ -2,6 +2,7 @@
 # ":set sort=random" or create a key binding with ":map oz set sort=random"
 
 import os
+import os.path as fs
 
 from ranger.container.directory import Directory
 
@@ -30,3 +31,75 @@ Directory.sort_dict["suffix_"] = lambda x: sort_suffix(x, "_", longest=False)
 #     cmd = ('du -bs' + path).split()
 #     return check_output(cmd).decode('utf-8').rstrip().split()[1]
 # Directory.sort_dict['dir_size'] = sort_dir_size
+
+try:
+    from functools import cache
+    from typing import cast
+
+    from pymediainfo import MediaInfo
+    from ranger.api import register_linemode
+    from ranger.core.linemode import DEFAULT_LINEMODE, LinemodeBase
+except Exception:
+    pass
+else:
+    # BET:PERF: read durations from /cache/irome_db
+    @cache
+    def get_duration_mediainfo(path: str) -> int:
+        """
+        Parses media file duration in milliseconds.
+        Returns: -1 on error, 1 if empty/zero, otherwise duration in ms.
+        """
+        try:
+            # ALT: $ find ... -print0 | xargs -0r mediainfo --Inform="General;%Duration%"$'\t'"%CompleteName%\n" -- >> withdur.txt
+            media_info = cast(MediaInfo, MediaInfo.parse(path))
+            for track in media_info.tracks:  # pyright: ignore[reportUnknownVariableType, reportUnknownMemberType]
+                if track.track_type == "General" and track.duration:  # pyright: ignore[reportUnknownMemberType]
+                    # MediaInfo returns duration in milliseconds (as a string or int)
+                    duration = int(track.duration)  # pyright: ignore[reportUnknownArgumentType, reportUnknownMemberType]
+                    return duration if duration > 0 else 1
+        except ValueError, TypeError, AttributeError, Exception:
+            return -1  # corrupt files or missing data
+        return -1
+
+    Directory.sort_dict["duration"] = lambda x: get_duration_mediainfo(x.path)
+
+    @register_linemode
+    class DurationLinemode(LinemodeBase):
+        name = "duration"
+
+        def filetitle(self, f, metadata):
+            return f.relative_path
+
+        def infostring(self, f, metadata):
+            if not f.is_file or not (f.video or f.audio):
+                return self._get_default_infostring(f, metadata)
+
+            ms = get_duration_mediainfo(f.path)
+            if ms <= 0:
+                return self._get_default_infostring(f, metadata)
+                # if ms < 0:
+                #     return "ERR"
+                # if ms == 0:
+                #     return "ZERO"
+            if ms <= 1:
+                return "WTF"
+            s, ms = divmod(ms, 1000)
+            m, s = divmod(s, 60)
+            return (f"{m}:" if m else "") + f"{s:02d}.{ms:03d}"
+
+        def _get_default_infostring(self, f, metadata):
+            # OR: return DefaultLinemode().infostring(f, metadata)
+
+            # 1. Fetch default mode string (usually "filename" or whatever DEFAULT_LINEMODE is)
+            default_mode_name = DEFAULT_LINEMODE
+
+            # Avoid recursion if DEFAULT_LINEMODE happens to be set to "duration"
+            if default_mode_name == self.name:
+                default_mode_name = "filename"
+
+            # 2. Retrieve instance from file's linemode dictionary
+            fallback_linemode = f.linemode_dict.get(default_mode_name)
+            if fallback_linemode:
+                return fallback_linemode.infostring(f, metadata)
+
+            return ""
