28190f615cff578fb2f71461cf0ecc4549c3ad83
Amerlyq <amerlyq+git@gmail.com>
2026-08-08 22:20:50 +0300

    [nvim] * chg ai_codecompanion_cfg.lua  +38

--- nvim/lua/plug/ai_codecompanion_cfg.lua
+++ nvim/lua/plug/ai_codecompanion_cfg.lua
@@ -286,3 +286,41 @@ vim.cmd([[cab C CodeCompanion]])
 --     },
 --   },
 -- })
+
+-- https://github.com/olimorris/codecompanion.nvim/discussions/1209
+-- local PROMPT_LIBRARY = {
+--   ['Improve Copy'] = {
+--     strategy = 'inline',
+--     description = 'Improves the copy of the page',
+--     -- opts = {
+--     --   mapping = '<LocalLeader>ch',
+--     -- },
+--     prompts = {
+--       {
+--         role = 'system',
+--         content = [[You are an experienced developer, designer and creator. Your build useful products that people love to use. Your task is to improve the copy of the content to bring more clarity and improve conversions.]],
+--       },
+--       {
+--         role = 'user',
+--         content = '#buffer\n\nPlease improve the copy of the page.',
+--       },
+--     },
+--   },
+--   ['Review the code'] = {
+--     strategy = 'inline',
+--     description = 'Review the code in buffer',
+--     -- opts = {
+--     --   mapping = '<LocalLeader>ch',
+--     -- },
+--     prompts = {
+--       {
+--         role = 'system',
+--         content = [[You are an experienced senior developer. You do code reviews.]],
+--       },
+--       {
+--         role = 'user',
+--         content = '#buffer\n\nPlease review the code in the file and make the fixes if any.',
+--       },
+--     },
+--   },
+-- }
