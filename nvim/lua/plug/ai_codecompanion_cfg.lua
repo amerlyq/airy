4388ed95b29f4f1ca4cddad3f5a855b628c30cbe
Amerlyq <amerlyq+git@gmail.com>
2026-09-06 14:35:32 +0300

    [%] * (M2 A1) ffmpeg,(+2): 3 files  +359/-369

--- nvim/lua/plug/ai_codecompanion_cfg.lua
+++ nvim/lua/plug/ai_codecompanion_cfg.lua
@@ -152,8 +152,8 @@ CC.setup({
           schema = {
             model = {
               -- default = "thinkingmachines/inkling:free",
-              default = "minimax/minimax-m3:free",
-              -- default = "nvidia/nemotron-3-ultra-550b-a55b:free",
+              -- default = "minimax/minimax-m3:free",
+              default = "nvidia/nemotron-3-ultra-550b-a55b:free",
               -- default = "poolside/laguna-s-2.1:free",
               -- default = "qwen/qwen-2.5-coder-32b-instruct:free",
               -- default = "anthropic/claude-sonnet-4.6"
